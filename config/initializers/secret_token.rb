b10d72c801a309df9ef19d52b96a927ec26e4e9a
Kevin Ball <kmball11@gmail.com>
2011-01-13 13:51:57 -0800

    Basics of a mysql-based app configured how I like 'em

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseappMysql::Application.config.secret_token = '14ce5a28b9d883be8bfcb271a518f2fc56e26483b264e2e4673fd2ea79027bed29c5d5c25ced9fbcc0962c34a31da0957a00fabbaeb5e31109da1ff825586684'
